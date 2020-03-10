cfb289c3594cf8cc4c5be8186e85b80e861a3609
Benumbed <benumbed@projectneutron.com>
2020-03-10 01:37:50 -0700

    Repositioning code

---
+++ nim_vault/src/nim_vault/bare/errors.nim
@@ -0,0 +1,8 @@
+## 
+## Errors for nim_vault
+##
+## (C) 2020 Benumbed (Nick Whalen) <benumbed@projectneutron.com> -- All Rights Reserved
+##
+import system
+
+type VaultError* = object of system.Exception
---
+++ nim_vault/src/nim_vault/structs.nim
@@ -0,0 +1,7 @@
+## 
+## Common structures for Vault API
+##
+## (C) 2020 Benumbed (Nick Whalen) <benumbed@projectneutron.com> -- All Rights Reserved
+##
+
+type StrWithError* = tuple[output: string, error: bool]
