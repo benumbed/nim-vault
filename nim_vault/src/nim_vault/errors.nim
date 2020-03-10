a27c9b45ed68cb38df9198a121a2ac9abe9860ea
Benumbed <benumbed@projectneutron.com>
2020-03-10 01:36:57 -0700

    Repositioning code and adding approle_login (no test yet)

--- nim_vault/src/nim_vault/errors.nim
+++
@@ -1,8 +0,0 @@
-## 
-## Errors for nim_vault
-##
-## (C) 2020 Benumbed (Nick Whalen) <benumbed@projectneutron.com> -- All Rights Reserved
-##
-import system
-
-type VaultError* = object of system.Exception
--- nim_vault/tests/test1.nim
+++ nim_vault/tests/test1.nim
@@ -1,32 +1,8 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-import json
-import os
-import strformat
-import unittest
-
-import nim_vault/connection
-
-suite "Test nim-vault API":
-
-    test "newConnection":
-        var vault_url = "http://localhost:8200"
-        # FIXME: This is a temp dev token, set this from the environment
-        var conn = newVaultConnection(vault_url)
-
-        check conn.vault_url == fmt"{vault_url}/v1"
-        check conn.vault_token == os.getEnv("VAULT_TOKEN")
-
-
-    test "test login":
-        var conn = newVaultConnection("http://localhost:8200")
-
-        var result = conn.login()
-
-        check result.kind == JObject
-        check result["errors"].kind == JArray
-        check result["errors"].len == 0
+## 
+## Initial tests for the Nim-based Vault API wrapper
+##
+## (C) 2020 Benumbed (Nick Whalen) <benumbed@projectneutron.com> -- All Rights Reserved
+##
+## NOTE: These tests assume you're running Vault locally on port 8200 (Docker).
+## In the future they will automatically spin a Vault docker container, once I port that code
+## over.
